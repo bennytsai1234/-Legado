29bffc6b4768ec4c3aa85113f582c899f988410f
BennyTsai1234 <bennytsai@gmail.com>
2026-03-10 16:25:10 +0800

    feat: initial Flutter project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# legado_reader
+
+An iOS novel reader inspired by Legado
---
+++ android/app/src/main/kotlin/com/legado/legado_reader/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.legado.legado_reader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
